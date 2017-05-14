921ff7106472b02fc344a44559fd64b741a4a095
kwoodard22 <kwoodard22@gmail.com>
2017-05-14 15:14:06 -0700

    Routes for identity, omniauth config, database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#show'
+
+  match "/auth/:provider/callback", to: "sessions#create"
+  match "/auth/failure", to: "sessions#failure"
+  match "/logout", to: "sessions#destroy", as: "logout"
+  resources :identities
+  
+  resources :listings
 end
