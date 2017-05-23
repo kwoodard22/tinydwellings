d2ad1736663ac013e903d5d71544dbc5add61530
kwoodard22 <kwoodard22@gmail.com>
2017-05-23 15:23:07 -0700

    Removed Identities to replace with Devise

--- app/controllers/identities_controller.rb
+++
@@ -1,7 +0,0 @@
-class IdentitiesController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :identity
-
-  def new
-    @identity = env['omniauth.identity']
-  end
-end
